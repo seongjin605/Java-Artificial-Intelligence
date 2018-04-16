a0682403f7412fa36cdb8342d576d5f5654d2b88
preciouStar <seongjin605@users.noreply.github.com>
2018-04-16 14:00:16 +0900

    TestTika
    주석 추가

--- JavaDataScience/src/com/java/chapter1/TestTika.java
+++ JavaDataScience/src/com/java/chapter1/TestTika.java
@@ -13,18 +13,33 @@ public class TestTika {
 
 		TestTika testTika = new TestTika();
 		testTika.convertPdf("C:/Users/admin/Desktop/testpdf.pdf");
-		System.out.println("");
 	}
 
 	public void convertPdf(String fileName) {
 		InputStream inputStream = null;
 		try {
 			inputStream = new FileInputStream(fileName);
+			//Apache Tika 제공 파서
 			AutoDetectParser parser = new AutoDetectParser();
+			
+			/*BodyContentHandler
+			 * 파일의 본문에 들어가있는 내용을 처리할 핸들러
+			 * Apache Tika는 최대 100,000자까지만 처리할수있게끔 제한되있으므로,
+			 * -1값을 매개변수로 넣어줌으로써 십만개 제약사항을 무시함.
+			 * */
+						
 			BodyContentHandler handler = new BodyContentHandler(-1);
 			Metadata metadata = new Metadata();
+			
+			/*
+			 *  스트림 : 문서 스트림 (입력) 
+			 *  handler : XHTML SAX 이벤트 용 처리기 (출력) 
+			 *  metadata : 문서 메타 데이터 (입력 및 출력) 
+			 *  context : 문맥 분석(컨텍스트 파싱)
+			 */
 			parser.parse(inputStream, handler, metadata, new ParseContext());
 			System.out.println(handler.toString());
+			//System.out.println("check");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
